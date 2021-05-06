316e62e5efd6894e5121ba493b73bb5bd865a37e
Luiz Fernando <lfaires@gmail.com>
2021-05-06 11:00:19 -0300

    implementação do layout do contador de quantidade

--- src/components/Beverages.js
+++ src/components/Beverages.js
@@ -1,9 +1,25 @@
+import React from "react"
+
 export default function Beverages() {
     const beveragesMenu = 
     [{beverage: "coke", dishes: "Refrigerante", altImages: "Coke can", description: "Lata 350 ml a partir de R$ 4,90", price: "4,90"},
     {beverage: "bud", dishes: "Budweiser", altImages: "Budweiser can", description: "350 ml da cerveja que conquistou o Brasil", price: "8,00"},
     {beverage: "mate", dishes: "Mate leão", altImages: "mate leão bottle and trees behind", description: "330 ml da bebida que é a cara do carioca", price: "6,75"}]
     
+    const [counter, setCounter] = React.useState(0)
+
+    function add() {
+        setCounter(counter+1)
+    }
+
+    function remove() {
+        if(counter === 0){
+            setCounter(0)
+        } else {
+        setCounter(counter-1)
+        }
+    }
+
     return (
         <div className="orders">
         <div className="orders-top">
@@ -12,7 +28,7 @@ export default function Beverages() {
         <div className="orders-body">
             <ul className="order-beverage">
                 {beveragesMenu.map((beverageMenu) => 
-                <li className={beverageMenu.beverage} onClick="activeOrderFood('.chicken')">   
+                <li className={beverageMenu.beverage} >   
                     <img src={"assets/images/" + beverageMenu.beverage + ".jpg"} alt={beverageMenu.altImages} />
                     
                     <div> <p className="dishes">{beverageMenu.dishes}</p> </div>
@@ -20,8 +36,14 @@ export default function Beverages() {
                     <div> <p className="description">{beverageMenu.description}</p> </div>
                         
                     <div>
-                        <span><span>&#82;&#36;&nbsp;</span><span className="price">{beverageMenu.price}</span></span>
-                        <ion-icon className="checkicon" name="checkmark-circle"></ion-icon>
+                        <div>
+                            <span><span>&#82;&#36;&nbsp;</span><span className="price">{beverageMenu.price}</span></span>
+                        </div>
+                        <div className="counter" >
+                            <div className="minus" ><ion-icon onClick={remove} name="remove-outline"></ion-icon></div>
+                            <div>{counter}</div>
+                            <div className="plus"><ion-icon onClick={add} name="add-outline"></ion-icon></div>
+                        </div>    
                     </div>
                 </li> )} 
             </ul>    
