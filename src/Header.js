715c0d598263742f6f38f89a3368517a344438a6
Luiz Fernando <lfaires@gmail.com>
2021-05-05 21:06:46 -0300

    componentização header e importação das fontes e do CSS

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header class="top">
+          <p class="heading">FoodCamp</p>
+          <p class="subheading">Sua comida em 6 minutos</p>
+        </header>
+    )
+}
