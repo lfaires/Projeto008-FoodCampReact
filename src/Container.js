f9b02d60a84826f50241c0ddf69d39aaada07564
Luiz Fernando <lfaires@gmail.com>
2021-05-05 23:10:12 -0300

    delete do componente orders

--- src/Container.js
+++ src/Container.js
@@ -1,9 +1,13 @@
-import Orders from "./components/Orders"
+import Foods from "./components/Foods"
+import Beverages from "./components/Beverages"
+import Desserts from "./components/Desserts"
 
 export default function Container() {  
     return (
         <div className="container">
-            <Orders />
+            <Foods />
+            <Beverages />
+            <Desserts />
         </div>
     )
 }
